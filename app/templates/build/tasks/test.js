a1acef1ac4cc49336029df84d08ecee077eb75d8
Franklin Wise <franklin@krave-n.com>
2014-12-06 20:11:14 -0800

    quotes changes

--- app/templates/build/tasks/test.js
+++ app/templates/build/tasks/test.js
@@ -1,21 +1,18 @@
 module.exports = function (grunt) {
-    'use strict';
+	'use strict';
 
-    var buildRequirejsConfig = function () {
-        var requirejsTemplate = grunt.file.read('build/requirejs.json');
-        return grunt.template.process(requirejsTemplate, { data : { path : '../' }});
-    };
+	var buildRequirejsConfig = function () {
+		var requirejsTemplate = grunt.file.read('build/requirejs.json');
+		//json uses double quotes, and we are using single quotes for all js
+		requirejsTemplate = requirejsTemplate.replace("\"", "'")
+		return grunt.template.process(requirejsTemplate, { data : { path : '../' }});
+	};
 
-    grunt.registerTask('buildTest', function () {
+	grunt.registerTask('buildTest', function () {
+		var testMainTemplate = grunt.file.read('build/test-main.js');
 
-        var testMainTemplate = grunt.file.read('build/test-main.js');
-
-        var data = {
-            requirejs : buildRequirejsConfig()
-        };
-
-        var processedIndex = grunt.template.process(testMainTemplate, { data : data });
-        grunt.file.write('test/unit/test-main.js', processedIndex);
-    });
-
+		var data = { requirejs : buildRequirejsConfig() };
+		var processedIndex = grunt.template.process(testMainTemplate, { data : data });
+		grunt.file.write('test/unit/test-main.js', processedIndex);
+	});
 };
